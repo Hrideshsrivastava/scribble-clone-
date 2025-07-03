a43f593c0b807bf97127e374cb0e474a97cb7431
Hrideshsrivastava <hrideshindia06@gmail.com>
2025-07-04 01:57:42 +0530

    adding front end link

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://scribble-clone-8l9k.vercel.app/",
     methods: ["GET", "POST"],
     
   },
